c6b486426e107f4f2811e37b57f8ce6935253fdf
Alejandra <alejandramelisalopez2@gmail.com>
2022-12-23 10:47:40 -0300

    added a scrollbar arrow in homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,7 @@ import gotLogoMobile from '../../assets/images/got_small_logo.png'
 import eddardStark from '../../assets/images/eddardstark.jpg'
 import westerosMap from '../../assets/images/westerosmap.jpg'
 import { Parallax, ParallaxLayer } from '@react-spring/web'
+import whiteArrow from '../../assets/images/white_arrow_down.png'
 
 const HomePage = () => {
 
@@ -27,14 +28,18 @@ const HomePage = () => {
                     <img class="img-fluid" className={styles.gotLogoDesktop} src={gotLogoDesktop} alt="" />
                     <img class="img-fluid" className={styles.gotLogoMobile} src={gotLogoMobile} alt="" />
                 </div>
+                <section  className={styles.whiteArrowContainer}>
+                    <a  className={styles.whiteArrowA}href="#section06"><span></span>
+                    </a>
+                </section>
             </div>
             <div className={styles.titleContainer}>
                 <div className={styles.optionBackground}>
-                    <h1 className={styles.title}>When you play the game of thrones, you win or you die. </h1>
+                    <h1 id="section06" className={styles.title}>When you play the game of thrones, you win or you die. </h1>
                     <h1 className={styles.title}>Who will sit on the Iron Throne in the end? </h1>
                     <div className={styles.optionBoxesContainer}>
                         <OptionBox image={eddardStark} text='Characters' link='/characters' />
-                        <OptionBox image={westerosMap} text='Houses' link='/houses'  />
+                        <OptionBox image={westerosMap} text='Houses' link='/houses' />
                     </div>
                 </div>
             </div>
